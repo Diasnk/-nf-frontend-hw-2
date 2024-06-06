97050496dddc8c93e9639fdf92d663506adfb8d4
Diasnk <diasnugumanov258@gmail.com>
2024-06-06 15:41:07 +0500

    2nd homework

--- app/components/TaskItem/index.jsx
+++ app/components/TaskItem/index.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
 import Image from 'next/image';
+import { FaRegCheckCircle, FaRegCircle } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
-const TaskItem = () => {
-  // Component that shows each TaskItem
+
+export default function TaskItem({ task, onToggleTask, onDeleteTask }) {
   return (
-    <></>
+    <li className="flex justify-between items-center p-2 bg-gray-600 rounded mb-2">
+      <div className="flex items-center">
+        <button 
+          className="w-6 h-6 my-auto mr-6 focus:outline-none"
+          onClick={() => onToggleTask(task.id)} 
+        >
+          {
+            task.completed 
+            ?
+              <FaRegCheckCircle 
+              className='text-gray-500 hover:text-white'
+              width={30}
+              height={30}
+              />
+            :
+              <FaRegCircle 
+              className='text-gray-500 hover:text-white'
+              width={30}
+              height={30}
+              />
+          } 
+        </button>
+        <span className={`ml-2 ${task.completed ? 'line-through text-gray-500' : 'text-white'}`}>{task.text}</span>
+      </div>
+      <button onClick={() => onDeleteTask(task.id)} className="text-gray-400 hover:text-white focus:outline-none">
+        <MdDelete />
+      </button>
+    </li>
   );
-};
-
-export default TaskItem;
+}
